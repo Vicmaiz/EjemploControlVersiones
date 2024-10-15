5c5300766f7a52384ae68af6b2eb6a935cac5023
Vicmaiz <vicmaiz@alumni.uv.es>
2024-10-15 13:15:30 +0200

    Perím Rectángulo y cambio pi

--- Perim.R
+++ Perim.R
@@ -2,6 +2,10 @@ PerimetroCuadrado<-function(L) {
   return(4*L)
 }
 
+PerimetroRectangulo<-function(L1, L2) {
+  return(2*(L1+L2))
+}
+
 LongitCircunf<-function(r) {
-  return(2*pi*r)
+  return(2*3.14159*r)
 }
